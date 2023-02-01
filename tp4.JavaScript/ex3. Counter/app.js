2e06437e70520db571279ed01cb6fee629c63245
Chhorn Kakada <lokpozzchhornkakada@gmail.com>
2023-02-01 13:29:46 +0700

    finished tp4 ex1,2,3

---
+++ tp4.JavaScript/ex3. Counter/app.js
@@ -0,0 +1,49 @@
+var displayNumber = document.getElementById("diplayNumber");
+
+class Counter {
+  number = 0;
+  constructor(number) {
+    this.number = number;
+  }
+
+  decrease() { this.number -= 1; }
+  increase() { this.number += 1; }
+  reset() { this.number = 0; }
+  getNumber() { return this.number; }
+  display() {
+    displayNumber.innerHTML = `${this.number}`;
+  }
+}
+
+let numberFromLocalstorage = parseInt(localStorage.getItem("counter")) || null;
+if (numberFromLocalstorage == null)  numberFromLocalstorage = 0; 
+var counter = new Counter(numberFromLocalstorage);
+
+counter.display();
+
+// decrease number
+const decreaseNumber = () => {
+  counter.decrease();
+  saveToLocalstorage(counter);
+  counter.display();
+}
+
+// increase number
+const increaseNumber = () => {
+  counter.increase();
+  saveToLocalstorage(counter);
+  counter.display();
+}
+
+// reset number
+const resetNumber = () => {
+  counter.reset();
+  saveToLocalstorage(counter);
+  counter.display();
+}
+
+// save value counter to localstorage
+const saveToLocalstorage = (counter) => {
+  localStorage.setItem("counter", `${counter.getNumber()}`);
+}
+
